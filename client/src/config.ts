4b0c944bb0ac7f98ab2b81d3a0325c28fd5ac712
itayz1e <itayz1e@gmail.com>
2023-12-10 22:17:44 +0200

    fix config.ts bug for deploy

--- client/src/config.ts
+++ client/src/config.ts
@@ -37,6 +37,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
