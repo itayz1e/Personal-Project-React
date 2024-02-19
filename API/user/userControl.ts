79948abdabfbf2d499e2704e9fbe854cc6695473
itayz1e <itayz1e@gmail.com>
2024-02-19 22:16:30 +0200

    fix bug v12

--- API/user/userControl.ts
+++ API/user/userControl.ts
@@ -29,7 +29,7 @@ export const login = async (req: any, res: any) => {
   try {
     const JWT_SECRET="sdsdgffdgdfasSFDFBDF"
     const secret = process.env.JWT_SECRET;
-    const { username, password } = req.body;
+    const { username } = req.body;
     const userDB = await UserModel.findOne({ username });
 
     if (!userDB) {
